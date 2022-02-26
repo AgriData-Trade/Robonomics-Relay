025ff4939c5153d07ef37d61bbce0a0d276a9934
Leon Bowie <leon@bowie-co.nz>
2022-02-27 06:30:29 +1300

    improve config logging

--- src/config.py
+++ src/config.py
@@ -12,11 +12,12 @@ class Config:
 
     def __init__(self):
         self._mnemonic = environ.get("MNEMONIC_SEED")
-        self.keypair = Keypair.create_from_mnemonic(self._mnemonic, ss58_format=32)
-        self.seed = self.keypair.seed_hex
-        self.database_url = environ.get("RELAY_DATABASE_URL")
-        self.mqtt_url = environ.get("MQTT_URL")
         logger.info(f"Configured with mnemonic: {self._mnemonic}")
+        self.keypair = Keypair.create_from_mnemonic(self._mnemonic, ss58_format=32)
+        logger.info(f"Configured with keypair: {self.seed}")
+        self.seed = self.keypair.seed_hex
         logger.info(f"Configured with seed: {self.seed}")
+        self.database_url = environ.get("RELAY_DATABASE_URL")
         logger.info(f"Configured with database_url: {self.database_url}")
+        self.mqtt_url = environ.get("MQTT_URL")
         logger.info(f"Configured with mqtt_url: {self.mqtt_url}")
